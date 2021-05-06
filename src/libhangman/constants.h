2469963851016e41094daeadf342ea81a7fb14e2
Vl1ts <vladooooos111@yandex.ru>
2021-05-06 23:49:43 +0700

    Add new values to constants.h

--- src/libhangman/constants.h
+++ src/libhangman/constants.h
@@ -1,8 +1,17 @@
 #pragma once
 
+#define button_thickness 1
+
+#define menu_button_length 300
+#define menu_button_height 100
+
 #define bg_color_r 255
 #define bg_color_g 235
 #define bg_color_b 200
+
+#define pr_button_r 236
+#define pr_button_g 223
+#define pr_button_b 255
 
 #define SUCCESS 0
 #define CANNOT_LOAD_FONT 999
