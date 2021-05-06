6f883f29a9470f78dc7a1f551fa121dff428059a
Vl1ts <vladooooos111@yandex.ru>
2021-05-06 22:28:43 +0700

    Move button struct to service.cpp

--- src/libhangman/service.h
+++ src/libhangman/service.h
@@ -2,4 +2,9 @@
 
 #include <SFML/Graphics.hpp>
 
+typedef struct {
+    sf::RectangleShape form;
+    sf::Text text;
+} st_button;
+
 bool is_window_closed(sf::RenderWindow& window, sf::Event event);
