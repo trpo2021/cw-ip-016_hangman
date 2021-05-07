0b2ee38be52a93f93ca682a23e01dbbe4c1cc331
Vl1ts <vladooooos111@yandex.ru>
2021-05-07 19:06:39 +0700

    Create header file to game_window.cpp

---
+++ src/libhangman/game_window.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <libhangman/constants.h>
+#include <libhangman/service.h>
+
+#include <SFML/Graphics.hpp>
+
+int open_game_window(sf::RenderWindow& window, int* choosen_theme);
