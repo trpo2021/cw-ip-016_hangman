fac26c0c9bc4cc6796823a5a54c42bbfa1295b2f
Vl1ts <vladooooos111@yandex.ru>
2021-05-08 00:14:48 +0700

    Add function to print alphabet buttons on the game window

--- src/libhangman/game_window.cpp
+++ src/libhangman/game_window.cpp
@@ -24,18 +24,21 @@ int open_game_window(sf::RenderWindow& window, int* choosen_theme)
     sf::Text text_theme;
     text_theme.setFont(font);
     text_theme.setString(choosen_theme_string);
-    text_theme.setStyle(sf::Text::Underlined);
     text_theme.setCharacterSize(70);
     text_theme.setFillColor(sf::Color::Black);
     text_theme.setOrigin(-400, -20);
+
+    st_button abc_buttons[abc_length];
 
     while (window.isOpen()) {
         window.clear(sf::Color(bg_color_r, bg_color_g, bg_color_b));
 
         window.draw(text_theme);
 
-	int word_size = choosen_word_string.size();
+        int word_size = choosen_word_string.size();
         draw_word_lines(window, &word_size);
+
+        draw_alphabet(window, abc_buttons);
 
         window.display();
 
