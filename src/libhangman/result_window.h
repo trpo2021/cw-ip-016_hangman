281f569888aae9b4e7c25199e48f7185372d7155
Vl1ts <vladooooos111@yandex.ru>
2021-05-15 22:02:37 +0700

    Create header file to result_window.cpp

---
+++ src/libhangman/result_window.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <libhangman/constants.h>
+#include <libhangman/service.h>
+#include <libhangman/theme_window.h>
+
+#include <SFML/Graphics.hpp>
+
+int open_result_window(
+        sf::RenderWindow& window, bool is_win, std::string* the_word);
