2e9fdedf87bfc6555b457958425090a459edfb52
Vl1ts <vladooooos111@yandex.ru>
2021-05-15 18:55:57 +0700

    Delete function printing word size lines, close iss #16

--- src/libhangman/draw.h
+++ src/libhangman/draw.h
@@ -5,8 +5,6 @@
 
 #include <SFML/Graphics.hpp>
 
-void draw_word_lines(sf::RenderWindow& window, int* word_size);
-
 void create_stand(sf::RectangleShape lines[]);
 
 void create_head(sf::CircleShape& head);
